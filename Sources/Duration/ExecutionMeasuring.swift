dfffbd2bd98818bb871f1ed443ba465f5c6c2006
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-03-03 13:34:17 +0100

    Support generic result for execution measurement (#22)

--- Sources/Duration/ExecutionMeasuring.swift
+++ Sources/Duration/ExecutionMeasuring.swift
@@ -10,38 +10,42 @@ import Foundation
 extension Duration {
     
     @inline(__always)
-    public static func measure(excutionDurationOf action: () throws -> Void) throws -> Duration {
+    public static func measure<T>(durationOf action: () throws -> T)
+    throws -> (duration: Duration, result: T) {
         let startTime = timestamp()
-        try action()
+        let result = try action()
         let endTime = timestamp()
-        return .seconds(endTime-startTime)
+        return (duration: .seconds(endTime-startTime), result: result)
     }
     
     @inline(__always)
-    public static func measure(excutionDurationOf action: () -> Void) -> Duration {
+    public static func measure<T>(durationOf action: () -> T) ->
+    (duration: Duration, result: T) {
         let startTime = timestamp()
-        action()
+        let result = action()
         let endTime = timestamp()
-        return .seconds(endTime-startTime)
+        return (duration: .seconds(endTime-startTime), result: result)
     }
     
     
     @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
     @inline(__always)
-    public static func measure(excutionDurationOf action: () async throws -> Void) async throws -> Duration {
+    public static func measure<T>(durationOf action: () async throws -> T)
+    async throws -> (duration: Duration, result: T) {
         let startTime = timestamp()
-        try await action()
+        let result = try await action()
         let endTime = timestamp()
-        return .seconds(endTime-startTime)
+        return (duration: .seconds(endTime-startTime), result: result)
     }
     
     @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
     @inline(__always)
-    public static func measure(excutionDurationOf action: () async -> Void) async -> Duration {
+    public static func measure<T>(durationOf action: () async -> T)
+    async -> (duration: Duration, result: T) {
         let startTime = timestamp()
-        await action()
+        let result = await action()
         let endTime = timestamp()
-        return .seconds(endTime-startTime)
+        return (duration: .seconds(endTime-startTime), result: result)
     }
     
     @inline(__always) static func timestamp() -> TimeInterval {
