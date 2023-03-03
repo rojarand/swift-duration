9738f109d22838c3208e98717642245f647624c9
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-03-03 15:48:52 +0100

    Convert int and doubles to duration (#21)

--- Sources/Duration/Int+Duration.swift
+++ Sources/Duration/Int+Duration.swift
@@ -7,11 +7,27 @@
 
 extension Int {
     
+    public var hours: Duration {
+        .hours(self)
+    }
+    
+    public var minutes: Duration {
+        .minutes(self)
+    }
+    
     public var seconds: Duration {
         .seconds(self)
     }
     
     public var milliseconds: Duration {
         .milliseconds(self)
+    }
+    
+    public var microseconds: Duration {
+        .microseconds(self)
+    }
+    
+    public var nanoseconds: Duration {
+        .nanoseconds(self)
     }
 }
