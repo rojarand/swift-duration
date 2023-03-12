998ed1cf5be8a36f92ff00677e4f5618e266cf88
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-03-12 07:47:08 +0100

    Apply 'rethrows' keyword for 'measure' API (#36)

--- Sources/Duration/ExecutionMeasuring.swift
+++ Sources/Duration/ExecutionMeasuring.swift
@@ -9,40 +9,19 @@ import Foundation
 
 extension Duration {
     
-    @inline(__always)
     public static func measure<T>(durationOf action: () throws -> T)
-    throws -> (duration: Duration, result: T) {
+    rethrows -> (duration: Duration, result: T) {
         let startTime = timestamp()
         let result = try action()
         let endTime = timestamp()
         return (duration: .seconds(endTime-startTime), result: result)
     }
     
-    @inline(__always)
-    public static func measure<T>(durationOf action: () -> T) ->
-    (duration: Duration, result: T) {
-        let startTime = timestamp()
-        let result = action()
-        let endTime = timestamp()
-        return (duration: .seconds(endTime-startTime), result: result)
-    }
-    
     @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-    @inline(__always)
     public static func measure<T>(durationOf action: () async throws -> T)
-    async throws -> (duration: Duration, result: T) {
+    async rethrows -> (duration: Duration, result: T) {
         let startTime = timestamp()
         let result = try await action()
-        let endTime = timestamp()
-        return (duration: .seconds(endTime-startTime), result: result)
-    }
-    
-    @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-    @inline(__always)
-    public static func measure<T>(durationOf action: () async -> T)
-    async -> (duration: Duration, result: T) {
-        let startTime = timestamp()
-        let result = await action()
         let endTime = timestamp()
         return (duration: .seconds(endTime-startTime), result: result)
     }
